8cde1927382ae122afc194f28ac7c45a923f669b
bat1skaf from the shkaf <108030989+bat1skaf@users.noreply.github.com>
2023-05-02 17:18:09 +0300

    Create 02-05-foreach.cpp

---
+++ 02-05-foreach.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    vector<string> nums { "one", "two", "three", "four", "five" };
+    int a = 0;
+    // while(a < nums.size()) {
+    for(string v : nums) {
+        if(a != 0) {
+            std::cout << ", ";
+        }
+        std::cout << nums[a];
+        a++;
+    }
+    return 0;
+}
